5d1daa434a3304bcfb425bb1859b5192175b64c8
yujichino <chino@jsl.co.jp>
2018-03-11 15:58:12 +0900

    Initial Commit

---
+++ sample-audio-player/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sample-audio-player
+//
+//  Created by 知野雄二 on 2018/03/11.
+//  Copyright © 2018年 知野雄二. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
